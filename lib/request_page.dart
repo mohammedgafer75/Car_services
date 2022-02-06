49c5d8dd216b7916ec8f42fc478d2cb36109e213
mohammedgafer75 <mohammedgafer75@gmail.com>
2022-02-06 15:17:53 +0200

    new

--- lib/request_page.dart
+++ lib/request_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class RequestPage extends StatefulWidget {
   RequestPage({Key? key, required this.table}) : super(key: key);
@@ -21,7 +22,7 @@ class _RequestPageState extends State<RequestPage> {
           child: StreamBuilder(
               stream: FirebaseFirestore.instance
                   .collection(widget.table)
-                  .where('aprrov', isEqualTo: 0)
+                  // .where('aprrov', isEqualTo: 0)
                   .snapshots(),
               builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
                 if (!snapshot.hasData) {
@@ -55,44 +56,95 @@ class _RequestPageState extends State<RequestPage> {
       elevation: 2,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       child: SizedBox(
-        height: 150,
+        height: 200,
         width: 200,
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Text(
-            'Request by: ${data['name']}',
+            'Request by:  ${data['name']}',
             style: const TextStyle(
                 fontWeight: FontWeight.bold, fontSize: 16, color: Colors.black),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 18, left: 18),
-            child: Text('Email: ${data['email']}',
+            child: Text('Email:  ${data['email']}',
                 style: const TextStyle(fontSize: 14, color: Colors.black)),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 18, left: 18),
-            child: Text('Number: ${data['phone']}',
+            child: Text('Number:  ${data['phone']}',
                 style: const TextStyle(fontSize: 14, color: Colors.black)),
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: TextButton(
-                style: ButtonStyle(
-                    padding: MaterialStateProperty.all(const EdgeInsets.only(
-                        top: 10, bottom: 10, left: 15, right: 15)),
-                    backgroundColor: MaterialStateProperty.all(
-                        const Color.fromRGBO(19, 26, 44, 1.0)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(13),
-                            side: const BorderSide(
-                                color: Color.fromRGBO(19, 26, 44, 1.0))))),
-                onPressed: () {
-                  accept(data.id);
-                },
-                child: const Text(
-                  'Accept',
-                  style: TextStyle(color: Colors.white),
-                )),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 18, left: 18),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Status: ',
+                      style: TextStyle(fontSize: 14, color: Colors.black)),
+                  data['aprrov'] == 0
+                      ? const Text('Rejected',
+                          style: TextStyle(fontSize: 14, color: Colors.red))
+                      : const Text('Accepted',
+                          style: TextStyle(fontSize: 14, color: Colors.green))
+                ],
+              ),
+            ),
+          ),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: TextButton(
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.only(
+                                  top: 10, bottom: 10, left: 15, right: 15)),
+                          backgroundColor: MaterialStateProperty.all(
+                              const Color.fromRGBO(19, 26, 44, 1.0)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(13),
+                                      side: const BorderSide(
+                                          color: Color.fromRGBO(
+                                              19, 26, 44, 1.0))))),
+                      onPressed: () {
+                        accept(data.id);
+                      },
+                      child: const Text(
+                        'Accept',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: TextButton(
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.only(
+                                  top: 10, bottom: 10, left: 15, right: 15)),
+                          backgroundColor: MaterialStateProperty.all(
+                              const Color.fromRGBO(19, 26, 44, 1.0)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(13),
+                                      side: const BorderSide(
+                                          color: Color.fromRGBO(
+                                              19, 26, 44, 1.0))))),
+                      onPressed: () {
+                        reject(data.id);
+                      },
+                      child: const Text(
+                        'Reject',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+              ],
+            ),
           )
         ]),
       ),
@@ -100,6 +152,9 @@ class _RequestPageState extends State<RequestPage> {
   }
 
   Future accept(String id) async {
+    setState(() {
+      showLoadingDialog(context);
+    });
     try {
       var res = await FirebaseFirestore.instance
           .collection(widget.table)
@@ -108,6 +163,7 @@ class _RequestPageState extends State<RequestPage> {
         "aprrov": 1,
       });
       setState(() {
+        Navigator.of(context).pop();
         showBar(context, 'Admin Accepted', 1);
       });
     } catch (e) {
@@ -116,6 +172,45 @@ class _RequestPageState extends State<RequestPage> {
         showBar(context, e.toString(), 0);
       });
     }
+  }
+
+  Future reject(String id) async {
+    setState(() {
+      showLoadingDialog(context);
+    });
+    try {
+      var res = await FirebaseFirestore.instance
+          .collection(widget.table)
+          .doc(id)
+          .update({
+        "aprrov": 0,
+      });
+      setState(() {
+        Navigator.of(context).pop();
+        showBar(context, 'Admin Rejected', 1);
+      });
+    } catch (e) {
+      setState(() {
+        Navigator.of(context).pop();
+        showBar(context, e.toString(), 0);
+      });
+    }
+  }
+
+  showLoadingDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) => const AlertDialog(
+        backgroundColor: Colors.transparent,
+        content: Center(
+          child: SpinKitFadingCube(
+            color: Colors.blue,
+            size: 50,
+          ),
+        ),
+      ),
+    );
   }
 
   void showBar(BuildContext context, String msg, int ch) {
